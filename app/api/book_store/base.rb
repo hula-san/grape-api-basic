8f18e098993cd122de5c391e937a9ac8e441f962
lanhnth <lanh.ptit.tn@gmail.com>
2018-08-03 10:35:52 +0700

    add return error

--- app/api/book_store/base.rb
+++ app/api/book_store/base.rb
@@ -1,5 +1,11 @@
 module BookStore
  class Base < Grape::API
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    message ={message: e.as_json}
+    error! message
+  end
+  
   mount BookStore::V1::Books
  end
 end
